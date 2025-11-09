720e8de1df13bf2e78fba16dcf710dbae77aa747
Baladitya Sai Chinni <119747533+Bunny825@users.noreply.github.com>
2025-11-09 14:06:08 +0530

    Update app_for_streamlit_cloud.py

--- app_for_streamlit_cloud.py
+++ app_for_streamlit_cloud.py
@@ -1,6 +1,9 @@
 import streamlit as st
 import time
-from langchain.chains import create_history_aware_retriever, create_retrieval_chain
+
+from langchain.chains import create_history_aware_retriever
+from langchain.chains import create_retrieval_chain
+
 from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain_community.chat_message_histories import AstraDBChatMessageHistory
 from langchain_core.chat_history import BaseChatMessageHistory
